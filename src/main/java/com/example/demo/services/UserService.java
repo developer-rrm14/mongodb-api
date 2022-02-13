fee67a64e8664c544ff28e7d25253e4a71cec531
Roberto Melo <robertoreboucasdemelo@gmail.com>
2022-02-13 15:15:38 -0300

    Add Get User By Id

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.example.demo.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.models.dto.UserDTO;
 import com.example.demo.models.entities.User;
 import com.example.demo.repositories.UserRepository;
@@ -21,5 +22,12 @@ public class UserService {
 		List<User> list = userRepository.findAll();
 		return list.stream().map(item -> new UserDTO(item)).collect(Collectors.toList());
 	}
+	
+	public UserDTO findById(String id) {
+		Optional<User> result = userRepository.findById(id);
+		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto Não Encontrado"));
+		return new UserDTO(entity);
+	}
+
 
 }
