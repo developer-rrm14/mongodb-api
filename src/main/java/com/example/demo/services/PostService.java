95bd5e1e85811dc2c9dc6cb6f5d592a54db84b3c
Roberto Melo <robertoreboucasdemelo@gmail.com>
2022-02-23 14:31:58 -0300

    Add Find By Title Pure Query

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -27,6 +27,7 @@ public class PostService {
 	
 	public List<PostDTO> findByTitle(String text){
 		List<Post> list = postRepository.findByTitleContainingIgnoreCase(text);
+		//List<Post> list = postRepository.searchTitle(text);
 		return list.stream().map(x-> new PostDTO(x)).collect(Collectors.toList());
 	}
 	
